d1ac112ef51f3d66f596b9de17c87143720acde9
anitaokoh <sleekymeeratura@gmail.com>
2020-12-12 21:08:34 +0100

    ETL, converted with lambda and refactored

--- Converting_to_audio_locally/extract_post.py
+++ Converting_to_audio_locally/extract_post.py
@@ -5,15 +5,21 @@ import json
 MEDIUM_API = "https://api.rss2json.com/v1/api.json"
 MEDIUM_HANDLER = "@anitaokoh"
 
-
+# get the article repsonse from teh API
 rss_url = "https://medium.com/feed/{}".format(MEDIUM_HANDLER)
 response = requests.get(url=MEDIUM_API, params={"rss_url": rss_url},)
 json_response = json.loads(response.text)
 
 def fetch_content(story_json):
+    """
+    Get the titles and description keys from the json response and store as a dictionary
+    """
     return {item['title']:item["description"] for item in story_json["items"]}
 
 def get_content():
+    """
+    Extract just the 7th article
+    """
     returned_content = fetch_content(json_response)
     content = list(returned_content.items())[7]
     return content
--- Converting_to_audio_locally/transform_post.py
+++ Converting_to_audio_locally/transform_post.py
@@ -1,12 +1,13 @@
 from extract_post import get_content
 import re
 
+#Extract text data and partition into title and body of content
 content = get_content()
 title = content[0]
 body = content[1]
 
 
-
+# List out the patterns in variables
 pattern_1 = '</pre>'
 pattern_2 = '\xa0|\u200a'
 pattern_3 = '(?s)<figure>.*?</figure>|(?s)<a href.*?>|(?s)<img src.*?>|</a>'
@@ -15,11 +16,12 @@ pattern_5 = '\d\D\s'
 pattern_6 = '\s(.yml)'
 pattern_7 = '(— all)'
 
+#Variables to use after cleaning the text
 body_beginning = '<speak><prosody volume="loud">'
 title_end = '</prosody> '
 body_end = '</speak>'
 
-
+# put the patterns to remove as well as the substitutes into a dictionary
 patterns = [pattern_1,pattern_2, pattern_3,pattern_4,pattern_5]
 replacement_1 = ['.</pre>',' ','','','' ]
 substitutions_1 = dict(zip(patterns,replacement_1))
@@ -32,6 +34,9 @@ replacement_2 = ['<p>', '</p>', '<amazon:effect name="drc">','</amazon:effect>',
 substitutions_2 = dict(zip(tags,replacement_2))
 
 def substitute_tags(body, substitutions_1):
+    """
+    Substitute the patterns identifies using the dictionary substitions_1
+    """
     new_body = body
     for i, v in substitutions_1.items():
         new_body = re.sub(i,v,new_body)
@@ -39,6 +44,9 @@ def substitute_tags(body, substitutions_1):
 
 
 def add_paragraph_tags(body):
+    """
+    Split text, find all the lines with no taga and add  p tags. Drop empty strings
+    """
     new = body.split('\n')
     new_list = []
     for i in new:
@@ -49,6 +57,9 @@ def add_paragraph_tags(body):
     return new_list
 
 def remove_commentary(body):
+    """
+    Remove line with publication-related information and remove lines with only closing tags 
+    """
     new_list = add_paragraph_tags(body)
     new = []
 
@@ -60,22 +71,34 @@ def remove_commentary(body):
     return new_string
 
 def replace(string, substitutions):
+    """
+    Substitute the patterns identifies using the dictionary substitions_2
+    """
 
     substrings = sorted(substitutions, key=len, reverse=True)
     regex = re.compile('|'.join(map(re.escape, substrings)))
     return regex.sub(lambda match: substitutions[match.group(0)], string)
 
 def add_ssml_to_keywords(output):
+    """
+    Add special ssml tags to words not pronounced well by Polly
+    """
     output = re.sub(pattern_6, '<say-as interpret-as="ordinal">.yml</say-as>', output)
     output = re.sub(pattern_7, '<say-as interpret-as="ordinal">—</say-as>all', output)
     return output
 
 def get_title_and_body_content(output):
+    """
+    Combine the title and the body along with a beginning and ending tag
+    """
     total_content = body_beginning +  title + title_end + output + body_end
     total_content = total_content.strip()
     return total_content
 
 def return_total_clean_content():
+    """
+    Run all functions
+    """
         body_new = substitute_tags(body, substitutions_1)
         body_clean = remove_commentary(body_new)
         output = replace(body_clean, substitutions_2)
